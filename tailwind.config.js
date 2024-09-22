c99ef9dd336fb6c001ad169a28b01b8e93c47b4f
Omar Ayman <49610154+Omar-Aymann@users.noreply.github.com>
2024-09-22 23:24:34 +0300

    first

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,vue}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        btnYellow: "#F0C71E",
+        lightYellow: "#FFFBE9",
+        themeColor: "#A61F35",
+        grayColor: "#dddddd"
+      }
+    },
+  },
+  plugins: [],
+}
